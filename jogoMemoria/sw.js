ecd2653a892e0662e138b27063bf04caa2ca3aaf
bonfim <fernandobonfimandrade@gmail.com>
2021-10-09 09:45:45 -0300

    ajust and fix

---
+++ jogoMemoria/sw.js
@@ -0,0 +1,33 @@
+const staticCacheName = 'site-static-v1';
+const assets = ['jogoMemoria/index.html',  
+'jogoMemoria/assets/font/ComicRelief-webfont.eot',
+'jogoMemoria/assets/font/ComicRelief-webfont.woff2',
+'jogoMemoria/assets/font/ComicRelief-webfont.woff',
+'jogoMemoria/assets/font/ComicRelief-webfont.ttf',
+'jogoMemoria/assets/memo.css'];
+self.addEventListener('install', evt => {
+    evt.waitUntil(caches.open(staticCacheName).then((cache) => {
+        console.log('Cacheando arquivos estaticos...');
+        cache.addAll(assets);
+    }));
+});
+self.addEventListener('activate', (e) => {
+    e.waitUntil(caches.keys().then((keyList) => {
+        return Promise.all(keyList.map((key) => {
+            if (key === staticCacheName) { return; }
+            return caches.delete(key);
+        }))
+    }));
+});
+self.addEventListener('fetch', (e) => {
+    e.respondWith((async() => {
+        const r = await caches.match(e.request);
+        console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+        if (r) { return r; }
+        const response = await fetch(e.request);
+        const cache = await caches.open(staticCacheName);
+        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+        cache.put(e.request, response.clone());
+        return response;
+    })());
+});
